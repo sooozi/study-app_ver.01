21620e58668d86915a0c62343766fccc7ea84522
kimsuji <skme1103@naver.com>
2023-12-05 22:40:20 +0900

    231205_스터디앱_폰트적용

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import "./fonts/Font.css";
 import reportWebVitals from './reportWebVitals';
 import GlobalStyle from './style/GlobalStyle';
 
--- src/style/GlobalStyle.js
+++ src/style/GlobalStyle.js
@@ -4,6 +4,9 @@ import reset from "styled-reset";
 const GlobalStyle = createGlobalStyle`
   ${reset}
 
+  body {
+    font-family: "Montserrat";
+  }
   #root {
     background-color: rgba(255, 219, 184, 1);
     display: flex;
