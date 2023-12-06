c084821d8c5892846ad0239b4ceed50cd5d2edea
kimsuji <skme1103@naver.com>
2023-12-06 09:00:54 +0900

    231206_스터디앱_css module 적용

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,35 +1,21 @@
 import styled from "styled-components";
+import textStyles from "../style/Text.module.css";
 
 const Wrapper = styled.div`
     height: inherit;
     padding-top: 3rem;
 `;
 
-const BigTxt = styled.h3`
-  color: rgb(57, 32, 5);
-  text-align: center;
-  font-size: 3rem;
-  font-weight: bolder;
-  line-height: 1.5;
-`;
-
-const GradaBigTxt = styled.h3`
-  background-image: linear-gradient(to left, ${(props) => props.leftColor} 30%, ${(props) => props.rightColor});
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: rgba(0,0,0,0);
-  text-align: center;
-  font-size: 3rem;
-  font-weight: bolder;
-  line-height: 1.5;
-`;
-
 function Home() {
     return (
       <>
         <Wrapper>
-          <BigTxt>Improve Your</BigTxt>
+          {/* <BigTxt>Improve Your</BigTxt>
           <GradaBigTxt rightColor="rgb(255, 175, 88)" leftColor="rgb(255, 65, 65)">Favorite Language</GradaBigTxt>
-          <BigTxt>With Natives</BigTxt>
+          <BigTxt>With Natives</BigTxt> */}
+          <h3 className={textStyles.bigTxt}>Improve Your</h3>
+          <h3 className={`${textStyles.gradientText}`} style={{ '--leftColor': 'rgb(255, 65, 65)', '--rightColor': 'rgb(255, 175, 88)' }}>Favorite Language</h3>
+          <h3 className={textStyles.bigTxt}>With Natives</h3>
         </Wrapper>
       </>
     );
