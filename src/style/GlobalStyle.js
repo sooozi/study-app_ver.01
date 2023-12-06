3bc6cf551e08118d3c3a93f4c041dfd91728d0f0
kimsuji <skme1103@naver.com>
2023-12-06 09:57:21 +0900

    231206_스터디앱_css module 적용

--- src/style/GlobalStyle.js
+++ src/style/GlobalStyle.js
@@ -1,14 +1,6 @@
-import { createGlobalStyle, css } from "styled-components";
+import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
-
-const bigTxt = css`
-  text-align: center;
-  font-size: 3rem;
-  font-weight: bolder;
-  letter-spacing: -0.1rem;
-  line-height: 1.5;
-  color: rgb(57, 32, 5);
-`;
+import './Text.module.css';
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
@@ -24,18 +16,6 @@ const GlobalStyle = createGlobalStyle`
     align-items: center;
     height: 100vh;
   }
-
-  .bigTxt {
-    ${bigTxt}
-  }
-
-  .gradientText {
-    ${bigTxt}
-    background-image: linear-gradient(to left, var(--leftColor) 30%, var(--rightColor));
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: rgba(0, 0, 0, 0);
-  }
-
 `;
 
 export default GlobalStyle;
