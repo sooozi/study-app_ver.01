5c14dfcef64a87093e363f4f97d81beab522a2ef
kimsuji <skme1103@naver.com>
2023-12-13 07:34:39 +0900

    231213_스터디앱_노트페이지 착수

--- src/pages/Memo.js
+++ src/pages/Memo.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import styled from "styled-components";
 import { Title } from '../components/Title';
+
+const MemoContainer = styled.div `
+  border: 1px solid red;
+`;
+
 
 function Memo() {
   return (
     <>
       <Title titleText="Let's" gradTitleText="Memo" iconBox="✏️" subTitleText="Great ideas start as notepad scribbles!" />
-      <input></input>
+      <MemoContainer>
+
+      </MemoContainer>
     </>
   );
 }
