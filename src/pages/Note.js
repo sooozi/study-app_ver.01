a34c387208a2568035aa8e8de37a2b314f901f6c
kimsuji <skme1103@naver.com>
2023-12-15 21:51:33 +0900

    231215_스터디앱_노트페이지 스토리지 저장

--- src/pages/Note.js
+++ src/pages/Note.js
@@ -5,7 +5,6 @@ import NotesBoard from '../components/NotesBoard';
 import { Title } from '../components/Title';
 
 const NoteContainer = styled.div `
-  min-height: 100vh; /* 100% 대신에 viewport 높이를 사용 */
   box-sizing: border-box; /* 박스 모델의 영향을 고려하여 box-sizing 설정 */
   display: flex;
   gap: 1rem;
@@ -18,8 +17,9 @@ function Note() {
       <Title titleText="Let's" gradTitleText="Notes" iconBox="✏️" subTitleText="Great ideas start with notes and scribbles!" />
       <NoteContainer>
         <NotesAdd>
-
+          
         </NotesAdd>
+
         <NotesBoard>
 
         </NotesBoard>
