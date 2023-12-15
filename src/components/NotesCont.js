da7a98b33f553df7c2e3121c942370146f1674f2
kimsuji <skme1103@naver.com>
2023-12-15 22:08:31 +0900

    231215_스터디앱_노트페이지 스토리지 저장된 리스트 보드에 불러오기

--- src/components/NotesCont.js
+++ src/components/NotesCont.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function NotesCont() {
+  const [memos, setMemos] = useState([]);
+
+  useEffect(() => {
+    // 로컬 스토리지에서 메모 데이터를 가져옵니다.
+    let storedMemos = JSON.parse(localStorage.getItem('memos')) || [];
+    
+    // 가져온 메모 데이터로 상태를 업데이트합니다.
+    setMemos(storedMemos);
+  }, []);
+
+  // 이제 'memos' 상태를 컴포넌트에서 렌더링하거나 다른 작업에 사용할 수 있습니다.
+
   return (
     <>
-
+      {/* 'memos' 상태를 필요에 따라 렌더링하거나 사용합니다. */}
+      {memos.map((memo) => (
+        <div key={memo.id}>
+          <h3>{memo.title}</h3>
+          <p>{memo.content}</p>
+          <p>{memo.date}</p>
+        </div>
+      ))}
     </>
   );
 }
-  
+
 export default NotesCont;
