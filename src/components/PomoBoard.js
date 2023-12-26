bcda754d061891d7ff0b01d60efffcc49f74ec07
kimsuji <skme1103@naver.com>
2023-12-26 20:55:30 +0900

    231226_스터디앱_뽀모도로_카운드다운

--- src/components/PomoBoard.js
+++ src/components/PomoBoard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from "styled-components";
 
 const PomoBoardWrap = styled.div `
@@ -16,11 +16,60 @@ const TimerWrap = styled.div `
 
 
 function PomoBoard() {
+    const [minutes, setMinutes] = useState(25);
+    const [seconds, setSeconds] = useState(0);
+    const [isActive, setIsActive] = useState(false);
+
+    useEffect(() => {
+        let interval;
+    
+        if (isActive) {
+          interval = setInterval(() => {
+            if (seconds === 0) {
+              if (minutes === 0) {
+                clearInterval(interval);
+                setIsActive(false);
+              } else {
+                setMinutes(minutes - 1);
+                setSeconds(59);
+              }
+            } else {
+              setSeconds(seconds - 1);
+            }
+          }, 1000);
+        } else {
+          clearInterval(interval);
+        }
+    
+        return () => clearInterval(interval);
+      }, [isActive, minutes, seconds]);
+
+      const startTimer = () => {
+        setIsActive(true);
+      };
+    
+      const pauseTimer = () => {
+        setIsActive(false);
+      };
+    
+      const resetTimer = () => {
+        setIsActive(false);
+        setMinutes(25);
+        setSeconds(0);
+      };
 
     return (
         <PomoBoardWrap>
             <TimerWrap>
-
+            <div>
+                <span>{String(minutes).padStart(2, '0')}:</span>
+                <span>{String(seconds).padStart(2, '0')}</span>
+            </div>
+            <div>
+                <button onClick={startTimer}>Start</button>
+                <button onClick={pauseTimer}>Pause</button>
+                <button onClick={resetTimer}>Reset</button>
+            </div>
             </TimerWrap>
         </PomoBoardWrap>
     );
