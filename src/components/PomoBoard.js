3f7a02c97d66f9ebc14aab4e21366585d549dcfa
kimsuji <skme1103@naver.com>
2023-12-26 20:49:52 +0900

    231226_스터디앱_뽀모도로_카운드다운 착수

---
+++ src/components/PomoBoard.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from "styled-components";
+
+const PomoBoardWrap = styled.div `
+  background: #fff;
+  padding: 1.2rem 1rem;
+  border-radius: 1.5rem;
+`;
+
+
+
+function PomoBoard() {
+
+    return (
+        <PomoBoardWrap>
+
+        </PomoBoardWrap>
+    );
+}
+
+export default PomoBoard;
--- src/pages/Pomodoro.js
+++ src/pages/Pomodoro.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import PomoBoard from '../components/PomoBoard';
 import { Title } from '../components/Title';
 
 function Pomodoro() {
     return (
       <>
         <Title titleText="Let's" gradTitleText="Pomodoro" iconBox="⏰" subTitleText="Change your day, one Pomodoro at a time!" />
-        <input></input>
+        <PomoBoard />
       </>
     );
   }
