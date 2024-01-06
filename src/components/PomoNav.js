c17e4aaad4aafab8938955cce2524c574cb64f30
kimsuji <skme1103@naver.com>
2024-01-06 20:32:19 +0900

    240106_스터디앱_뽀모도로_스타일수정

--- src/components/PomoNav.js
+++ src/components/PomoNav.js
@@ -26,39 +26,6 @@ const InnerTit = styled.span `
     margin-bottom: 10px;
 `;
 
-const InnerTask = styled.div `
-    margin: 10px 0 0;
-`;
-
-const TaskBtn= styled.button `
-    width: 100%;
-    height: 30px;
-    padding: 0 0.5rem;
-    overflow: hidden;
-    border-radius: 5px;
-    border: 1px solid #ffdbb8;
-    background-color: #fff;
-    cursor: pointer;
-    font-size: 12px;
-    font-family: 'Montserrat';
-    font-weight: 500;
-    letter-spacing: -0.5px;
-    color: rgb(57, 32, 5);
-    text-align: left;
-    transition: all 0.2s ease-in-out;
-    &:not(:last-child) {
-        margin-bottom: 5px;
-    }
-    &:hover {
-        border: 1px solid rgba(255, 243, 229, 1);
-        background-color: rgba(255, 243, 229, 1);
-    }
-    &.on {
-        border: #ffe0bc;
-        background-color: #ffe0bc;
-    }
-`;
-
 const ContWrap= styled.div `
     display: flex;
     flex-direction: column;
