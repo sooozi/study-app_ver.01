c69f894c854734fe5c3247aab42c0322e711bbae
kimsuji <skme1103@naver.com>
2024-01-13 15:36:49 +0900

    240113_스터디앱_모바일_햄버거 아이콘

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -45,80 +45,75 @@ const HamburgerMenu = styled.button`
 `;
 
 const HamburgerMenuIcon = ({ isOpen, onClick }) => (
-    <svg
-      width="18"
-      height="18"
-      viewBox="0 0 18 18"
-      onClick={onClick}
+  <svg width="18" height="18" viewBox="0 0 18 18" onClick={onClick}>
+    <polyline
+      id="globalnav-menutrigger-bread-bottom"
+      className={`globalnav-menutrigger-bread globalnav-menutrigger-bread-bottom ${isOpen ? 'open' : ''}`}
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      points={isOpen ? "3 15, 15 3" : "2 12, 16 12"}
     >
-      <polyline
-        id="globalnav-menutrigger-bread-bottom"
-        className={`globalnav-menutrigger-bread globalnav-menutrigger-bread-bottom ${isOpen ? 'open' : ''}`}
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="1.2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        points="2 12, 16 12"
-      >
-        <animate
-          id="globalnav-anim-menutrigger-bread-bottom-open"
-          attributeName="points"
-          keyTimes="0;0.5;1"
-          dur="0.24s"
-          begin="indefinite"
-          fill="freeze"
-          calcMode="spline"
-          keySplines="0.42, 0, 1, 1;0, 0, 0.58, 1"
-          values=" 2 12, 16 12; 2 9, 16 9; 3.5 15, 15 3.5"
-        ></animate>
-        <animate
-          id="globalnav-anim-menutrigger-bread-bottom-close"
-          attributeName="points"
-          keyTimes="0;0.5;1"
-          dur="0.24s"
-          begin="indefinite"
-          fill="freeze"
-          calcMode="spline"
-          keySplines="0.42, 0, 1, 1;0, 0, 0.58, 1"
-          values=" 3.5 15, 15 3.5; 2 9, 16 9; 2 12, 16 12"
-        ></animate>
-      </polyline>
-      <polyline
-        id="globalnav-menutrigger-bread-top"
-        className={`globalnav-menutrigger-bread globalnav-menutrigger-bread-top ${isOpen ? 'open' : ''}`}
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="1.2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        points="2 5, 16 5"
-      >
-        <animate
-          id="globalnav-anim-menutrigger-bread-top-open"
-          attributeName="points"
-          keyTimes="0;0.5;1"
-          dur="0.24s"
-          begin="indefinite"
-          fill="freeze"
-          calcMode="spline"
-          keySplines="0.42, 0, 1, 1;0, 0, 0.58, 1"
-          values=" 2 5, 16 5; 2 9, 16 9; 3.5 3.5, 15 15"
-        ></animate>
-        <animate
-          id="globalnav-anim-menutrigger-bread-top-close"
-          attributeName="points"
-          keyTimes="0;0.5;1"
-          dur="0.24s"
-          begin="indefinite"
-          fill="freeze"
-          calcMode="spline"
-          keySplines="0.42, 0, 1, 1;0, 0, 0.58, 1"
-          values=" 3.5 3.5, 15 15; 2 9, 16 9; 2 5, 16 5"
-        ></animate>
-      </polyline>
-    </svg>
-  );
+      <animate
+        id="globalnav-anim-menutrigger-bread-bottom-open"
+        attributeName="points"
+        keyTimes="0;0.5;1"
+        dur="0.4s"
+        begin="indefinite"
+        fill="freeze"
+        calcMode="spline"
+        keySplines="0.42, 0, 0.58, 1;0, 0, 0.58, 1"
+        values={isOpen ? " 3 15, 15 3; 9 9, 9 9" : " 2 12, 16 12; 2 9, 16 9; 3.5 15, 15 3.5"}
+      ></animate>
+      <animate
+        id="globalnav-anim-menutrigger-bread-bottom-close"
+        attributeName="points"
+        keyTimes="0;0.5;1"
+        dur="0.4s"
+        begin="indefinite"
+        fill="freeze"
+        calcMode="spline"
+        keySplines="0.42, 0, 0.58, 1;0, 0, 0.58, 1"
+        values={isOpen ? " 9 9, 9 9; 3 15, 15 3" : " 3.5 15, 15 3.5; 2 9, 16 9; 2 12, 16 12"}
+      ></animate>
+    </polyline>
+    <polyline
+      id="globalnav-menutrigger-bread-top"
+      className={`globalnav-menutrigger-bread globalnav-menutrigger-bread-top ${isOpen ? 'open' : ''}`}
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      points={isOpen ? "3 3, 15 15" : "2 5, 16 5"}
+    >
+      <animate
+        id="globalnav-anim-menutrigger-bread-top-open"
+        attributeName="points"
+        keyTimes="0;0.5;1"
+        dur="0.4s"
+        begin="indefinite"
+        fill="freeze"
+        calcMode="spline"
+        keySplines="0.42, 0, 0.58, 1;0, 0, 0.58, 1"
+        values={isOpen ? " 3 3, 15 15; 9 9, 9 9" : " 2 5, 16 5; 2 9, 16 9; 3.5 3.5, 15 15"}
+      ></animate>
+      <animate
+        id="globalnav-anim-menutrigger-bread-top-close"
+        attributeName="points"
+        keyTimes="0;0.5;1"
+        dur="0.4s"
+        begin="indefinite"
+        fill="freeze"
+        calcMode="spline"
+        keySplines="0.42, 0, 0.58, 1;0, 0, 0.58, 1"
+        values={isOpen ? " 9 9, 9 9; 3 3, 15 15" : " 3.5 3.5, 15 15; 2 9, 16 9; 2 5, 16 5"}
+      ></animate>
+    </polyline>
+  </svg>
+);
 
 const UserWrap = styled.div`
     display: flex;
